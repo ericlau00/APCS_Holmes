c0a599f3325da302501ab057a11f52b67b5a3f02
elau00@cslab1-20 <elau00@stuy.edu>
2019-02-27 13:22:53 -0500

    update from school

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 	
 	public static void main(String[] commandLine) {
 		System.out.println("hello, world");
-        System.out.println("update");
+        	System.out.println("update");
+		System.out.println("update from school");
 	}
 }
