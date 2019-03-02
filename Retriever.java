d9a07ba8f330bcbd5c8f7137ffbacd7fe18dec06
Saad Bhuiyan <ssaad00@stuy.edu>
2019-03-02 14:53:59 -0500

    Saad's Changes

--- Retriever.java
+++ Retriever.java
@@ -1,3 +1,5 @@
+// Retriever's are nice but I want a husky. - Saad
+
 public class Retriever extends Pet{
     public String speak() {
         return "arf";
--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,3 +1,5 @@
+// I think you misspelled dog. - Saad 
+
 public class UserOfSpeakers{
     public static void main(String[] commandLine) {
         Animal doggo = new Retriever();
