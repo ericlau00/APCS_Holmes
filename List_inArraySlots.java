c8f273c741238e7e018e64f3f6d4cc2f02891470
ericlau00 <sharkguy418@gmail.com>
2019-03-06 21:16:06 -0500

    partially completeted homework

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -6,29 +6,37 @@
 public class List_inArraySlots {
 
     // declare fields here
-  int[] refArray;
-  int filledElements;
+    int[] refArray;
+    int filledElements;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+        refArray = new int[10];
+        filledElements = 0;
     }
 
 
     /** 
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
-
+    public int size() {
+        return filledElements;
+    }
 
      /** 
        @return a string representation of this list,
        in [a,b,c,] format
       */ 
-    // public String toString() {
-    // }
+    public String toString() {
+        String representation = "[";
+        for(int currentElement = 0; currentElement < filledElements; currentElement++) {
+            representation += refArray[currentElement] + ",";
+        }
+        representation += "]";
+        return representation;
+    }
 
     
     /** 
@@ -36,8 +44,10 @@ public class List_inArraySlots {
       
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int value) {
-     // }
+     public boolean add( int value) {
+         refArray[filledElements++] = value;
+         return true;
+     }
 
 
     /** 
