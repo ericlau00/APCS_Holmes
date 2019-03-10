4418a3dc0115f7b54f737adbb8ff35371f4c95de
ericlau00 <sharkguy418@gmail.com>
2019-03-09 23:54:44 -0500

    added functional get, set, and remove methods, along with tests

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -72,4 +72,58 @@ public class List_inArraySlots {
             bigger[ elemIndex] = elements[ elemIndex];
         elements = bigger;
      }
+     
+    /**
+      accessor
+      @return element @index from this list
+      precondition: @index is within the bounds of the array.
+          (Having warned the user about this precondition,
+           you should NOT complicate your code to check
+           whether user violated the condition.)
+     */
+    public int get( int index ) {
+        return elements[index];
+    }
+    
+    /**
+      Set value at @index to @newValue
+
+      @return old value at @index
+      @precondition: @index is within the bounds of this list.
+     */
+    public int set( int index, int newValue ) {
+        int oldValue = elements[index];
+        elements[index] = newValue;
+        return oldValue;
+    }
+
+
+    /**
+      Insert @value at position @index in this list.
+
+      Shift the element currently at that position (if any)
+      and any subsequent elements to the right
+      (that is, increase the index associated with each).
+     */
+     // public void add( int index, int value) {
+         
+     // }
+
+
+    /**
+      Remove the element at position @index in this list.
+
+      Shift any subsequent elements to the left (that is,
+      decrease the index associated with each).
+
+      @return the value that was removed from the list
+     */
+     public int remove( int index) {
+         int removedValue = elements[index];
+         for(int position = index; position < filledElements; position++) {
+             elements[position] = elements[position + 1];
+         }
+         elements[filledElements--] = 0;
+         return removedValue; 
+     }
 }
