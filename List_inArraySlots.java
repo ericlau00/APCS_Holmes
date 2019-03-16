488f97a1820b1d7ccea19740e3a115668509edb5
ericlau00 <sharkguy418@gmail.com>
2019-03-16 19:44:00 -0400

    updated skeleton

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,20 +1,19 @@
 /**
-  Implement a list of diverse types, including
-  integers, double-precision floating point numbers,
-  and Strings.
+  Implement a list of diverse types.
  */
 
 public class List_inArraySlots {
 
-    private Object[] objectElements;
     private int filledElements; // the number of elements in this list
+    private Object[] list;
+
     private static final int INITIAL_CAPACITY = 10;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-        objectElements = new Object[INITIAL_CAPACITY];
+      list = new Object[ INITIAL_CAPACITY];
     }
 
 
@@ -22,7 +21,7 @@ public class List_inArraySlots {
       @return the number of elements in this list
      */
     public int size() {
-        return filledElements;
+      return filledElements;
     }
 
 
@@ -31,13 +30,13 @@ public class List_inArraySlots {
        in [a,b,c,] format
       */
     public String toString() {
-        String representation = "[";
-        for(int element = 0; element < filledElements; element++) {
-            representation += objectElements[element];
-            representation += ",";
-        }
-        representation += "]";
-        return representation;
+      String stringRep = "[";
+
+      for (int index = 0; index < filledElements; index++)
+        stringRep += list[index] + ",";
+
+      stringRep += "]";
+      return stringRep;
     }
 
 
@@ -46,9 +45,10 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-    public boolean add(Object object) {
-        if (filledElements == objectElements.length) expand();
-        objectElements[filledElements++] = object;
+     public boolean add( Object val) {
+        // expand when at max
+        if (filledElements == list.length) expand();
+        list[ filledElements++] = val;
         return true;
      }
 
@@ -58,21 +58,19 @@ public class List_inArraySlots {
       preserving existing data.
      */
      private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           /* S.O.P. rules for debugging:
-              Working methods should be silent. But during
-              development, the programmer must verify that
-              this method is called when that is appropriate.
-              So test using the println(), then comment it out.
-              */
-        Object[] newObject = new Object[objectElements.length * 2];
-        for(int index = 0; index < filledElements; index++) {
-            newObject[index] = objectElements[index];
-        }
-        objectElements = newObject;
+        // create new list, copy values over, and replace list
+        Object[] bigger = new Object[list.length * 2];
+
+        // copy existing elements
+        for(int index=0; index<list.length; index++)
+            bigger[index] = list[index];
+
+        list = bigger;
      }
-     
-     public Object get(int index) {
-         return objectElements[index];
-     }
+
+
+     public Object get( int index){
+        // pass-through request
+        return list[ index];
+    }
 }
--- UserOfList.java
+++ UserOfList.java
@@ -3,45 +3,44 @@
  */
 public class UserOfList {
     private static List_inArraySlots list;
-    
+
     public static void main( String[] args ) {
         list = new List_inArraySlots();
 
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "empty list: " + list);
+        // Populate the list with values from the assignment
+        System.out.println(
+          "Populate the list with values from the assignment");
+        list.add( "happy pi day");
+        list.add( "piazza");
+        list.add( 3.14);
+        list.add( "pi");
+        list.add( 256);
+        list.add( Double.POSITIVE_INFINITY);
+        list.add( "tau");
+        list.add( "pizza");
+        list.add( 1e4);
+        list.add( Double.NEGATIVE_INFINITY);
 
-        // Populate the list with diverse elements.
-        list.add("important");
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "added string: " + list);
+        System.out.println("the sample list: "
+          + list.size() + " elements:" + System.lineSeparator()
+          + list                       + System.lineSeparator()
+          );
 
-        list.add(17);
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "added int: " + list);
+        // pi-prefixed phrases
+        System.out.println(
+            "pi-prefixed phrases: "
+          + Accumulator.catElementsStartingWith( list, "pi")
+          + System.lineSeparator());
 
-        list.add(1.618);
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "added double: " + list);
-        
-        list.add(true);
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "added boolean: " + list);
-        
+        // // list of finite Doubles
+        // List_inArraySlots numbers = Accumulator.finites( list);
+        // System.out.println( "list of finite Doubles: "
+           // + numbers.size() + " elements:" + System.lineSeparator()
+           // + numbers
+           // + System.lineSeparator());
 
-        // Add enough elements that expansion is expected
-        for( int elemIndex = list.size(); elemIndex < 15; elemIndex++ ) {
-
-            if( list.size() == 10) System.out.println( "expansion expected");
-
-            list.add(-elemIndex);
-            System.out.println( "number of elements: " + list.size() );
-        }
-        System.out.println("result of expanded list: "
-             + list.size() + " elements:");
-        System.out.println( list + System.lineSeparator());
-        
-        for(int index = list.size() - 1; index >= 0; index--) {
-            System.out.println("index: " + index + " value: " + list.get(index));
-        }
+        // // test ?? template
+        // System.out.println( "test ??");
+        // System.out.println( list + System.lineSeparator());
     }
 }
