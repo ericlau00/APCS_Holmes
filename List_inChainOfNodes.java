7e2432fb50a692805487158e93a9d5f14a1ca9c1
ericlau00 <sharkguy418@gmail.com>
2019-03-26 04:44:08 -0400

    functional get and set methods

---
+++ List_inChainOfNodes.java
@@ -0,0 +1,129 @@
+/**
+  Represent a list, implemented in a chain of nodes
+ */
+
+public class List_inChainOfNodes{
+    private Node headReference;
+
+    /**
+      Construct an empty list
+     */
+     
+    /**
+      @return the number of elements in this list
+     */
+    public int size() {
+        return size(headReference);
+    }
+    
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        else {
+            return 1 + size(node.getReferenceToNextNode());
+        }
+    }
+
+     /**
+       @return a string representation of this list,
+       format:
+           # elements [element0,element1,element2,] 
+      */
+    public String toString() {
+        String rep = Integer.toString(this.size()) + " elements [";
+        rep += toString(headReference);
+        rep += "]";
+        return rep;
+    }
+    
+    private String toString(Node node) {
+        if (node == null) {
+            return "";
+        }
+        else {
+            return node.getCargoReference().toString() + "," 
+                 + toString(node.getReferenceToNextNode());
+        }
+    }
+    
+    /**
+      Append @value to the head of this list.
+
+      @return true, in keeping with conventions yet to be discussed
+     */
+    public boolean addAsHead( Object val) {
+        Node newNode = new Node(val);
+        newNode.setReferenceToNextNode(headReference);
+        headReference = newNode; 
+        return true;
+    }
+    
+    /**
+      accessor
+      @return element @index from this list
+      precondition: @index is within the bounds of the array.
+          (Having warned the user about this precondition,
+           you should NOT complicate your code to check
+           whether user violated the condition.)
+     */
+    public Object get( int index ) {
+        int current = 0;
+        Node currentNode = headReference;
+        Object getThis = new Object(); 
+        while (currentNode != null) {
+            if (current == index) {
+                getThis = currentNode.getCargoReference();
+            }
+            current++;
+            currentNode = currentNode.getReferenceToNextNode();
+        }
+        return getThis;
+    }
+    
+    /**
+      Set value at @index to @newValue
+
+      @return old value at @index
+      @precondition: @index is within the bounds of this list.
+     */
+    public Object set( int index, int newValue ) {
+        Object oldValue = new Object();
+        Node newNode = new Node(newValue);
+        if (index == 0) {
+            oldValue = headReference.getCargoReference();
+            newNode.setReferenceToNextNode(headReference.getReferenceToNextNode());
+            headReference = newNode;
+        }
+        else {
+            int current = 0;
+            Node currentNode = headReference;
+            while (currentNode != null) {
+                if (current == index - 1) {
+                    oldValue = currentNode.getReferenceToNextNode().getCargoReference();
+                    newNode.setReferenceToNextNode(currentNode.getReferenceToNextNode().getReferenceToNextNode());
+                    currentNode.setReferenceToNextNode(newNode);
+                }
+                current++;
+                currentNode = currentNode.getReferenceToNextNode();
+            }
+        }
+        return oldValue;
+    }
+    
+    // /**
+      // Insert @value at position @index in this list.
+     // */
+    // public void add( int index, int value) {
+
+    // }
+    
+     // /**
+      // Remove the element at position @index in this list.
+
+      // @return the value that was removed from the list
+     // */
+    // public int remove( int index) {
+
+    // }
+}
