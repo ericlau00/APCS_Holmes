ad057e1d4189cd3a9f85ee113e6d869e98552fe7
ericlau00 <sharkguy418@gmail.com>
2019-04-03 19:46:59 -0400

    added OrderedList interface

--- OrderedList_inArraySlots.java
+++ OrderedList_inArraySlots.java
@@ -6,7 +6,7 @@
   List_inArraySlots
  */
 
-public class OrderedList_inArraySlots {
+public class OrderedList_inArraySlots implements OrderedList {
 
     private List_inArraySlots list_iAS;
 
@@ -26,12 +26,18 @@ public class OrderedList_inArraySlots {
       Put @value where it belongs in the list.
       @pre: the list is in increasing order
      */
-     public void add( int value) {
+    public void add( int value) {
          int dest = 0;
          for( ; dest < list_iAS.size() && list_iAS.get( dest) < value
             ; dest++) ;
          // System.out.println( "OL adding " + value
          //                   + " at index " + dest);
          list_iAS.add( dest, value);
-     }
+    }
+    
+    public int remove(int index) { return list_iAS.remove(index); }
+    
+    public int get(int index) { return list_iAS.get(index); }
+    
+    public int size() { return list_iAS.size(); } 
 }
