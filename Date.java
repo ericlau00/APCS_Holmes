0ed1fab02d6f58229706c059963019cf80c3ce88
ericlau00 <sharkguy418@gmail.com>
2019-04-04 18:23:00 -0400

    skeleton

---
+++ Date.java
@@ -0,0 +1,24 @@
+/**
+  Represent a date
+ */
+public class Date{
+    private int y,m,d;
+    
+
+    // -------- previously-written code --------
+    // constructor
+    public Date( int y, int m, int d) {
+        this.y = y;
+        this.m = m;
+        this.d = d;
+    }
+    
+    /**
+      @return a string representation of this instance
+     */
+    public String toString() {
+        // someday: ISO 8601
+        return y + "-" + m + "-" + d;
+    }
+
+}
