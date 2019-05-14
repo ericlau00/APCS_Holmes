b42ad628fa23b93d7b4e06bf2f9488be145a3ca1
ericlau00 <sharkguy418@gmail.com>
2019-05-14 02:41:27 -0400

    write recurrence

--- Fib.java
+++ Fib.java
@@ -11,10 +11,16 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+        if(n < 3) //binary decision between base cases and recursive cases 
+            return 1; //solution to the base cases. 
+        else 
+            return 
+                fib_recurrence(n-1) //invocation of the recursive abstraction 
+                + //combination process 
+                fib_recurrence(n-2); //invocation of the recursive abstraction 
     }
-     /* These are class / instance methods because...
+     /* These are class methods because the output of the methods will be the same for all instances of Fib. 
     */
 
 
@@ -22,8 +28,8 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+
     }
     /* Time complexity:
        Consider the size of the problem to be ...
@@ -43,7 +49,7 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
+    public static double fib_Binet( int n) {
         return -2; // invalid Fibonacci number
     }
     /* Time complexity:
