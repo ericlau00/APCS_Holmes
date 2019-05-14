41ba3fe8600b2675ed3b2173035a87b00b64d23f
ericlau00 <sharkguy418@gmail.com>
2019-05-14 03:27:01 -0400

    finito

--- Fib.java
+++ Fib.java
@@ -7,6 +7,19 @@
  */
 
 public class Fib {
+    
+    public static void main(String[] commandLine) {
+        //recurrence ~40
+        //grade8 ~400 435 
+        //binet ~400 453
+        Fib fib = new Fib();
+        long start = System.currentTimeMillis();
+        for(int n = 1; n<401; n++) {
+            System.out.println(n + ": " + fib.fib_Binet(n));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
     /** 
       @return the nth Fibonacci number
               by implementing the recurrence relation
@@ -29,17 +42,25 @@ public class Fib {
               calculated via the 8th-grade algorithm
      */
     public static long fib_grade8( int n) {
-
+        long sub_2 = 0;
+        long sub_1 = 1;
+        for(int i = 1; i < n; i++) {
+            long storage = sub_1;
+            sub_1 += sub_2; 
+            sub_2 = storage; 
+        }
+        return sub_1; 
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be linear.
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count number of iterations. 
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the the grade8 algorithm
+       grows linearly 
        as the size of the problem increases,
-       because ??
+       because as the n increases by an additional amount, the number of loops goes up
+       by that additional amount. 
      */
 
 
@@ -50,16 +71,18 @@ public class Fib {
               ignore rounding issues.
      */
     public static double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+        double phi = (1 + Math.sqrt(5)) / 2;
+        double psi = (1 - Math.sqrt(5)) / 2;
+        return (Math.pow(phi,n) - Math.pow(psi,n)) / Math.sqrt(5);
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be constant. 
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count number of computations.
        
-       Then cost of the the recurrence algorithm
-       grows ?? 
+       Then cost of the the binet algorithm
+       grows not 
        as the size of the problem increases,
-       because ??
+       because the number of computations stays constant. 
      */
 }
