b472d3edb10b5a572a068cfaa6e5fdc3ca838cac
ericlau00 <sharkguy418@gmail.com>
2019-06-15 02:07:26 -0400

    more mazes

--- UserOfSolver.java
+++ UserOfSolver.java
@@ -1,8 +1,10 @@
 public class UserOfSolver {
     public static void main(String[] commandLine) {
-        MazeSolver solve = new MazeSolver("maze.txt");
+        MazeSolver solve = new MazeSolver("maze0.txt");
         System.out.println(solve);
         solve = new MazeSolver("maze1.txt");
+        System.out.println(solve);
+        solve = new MazeSolver("maze2.txt");
         System.out.println(solve);
     }
 }
