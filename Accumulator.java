5029724f19920fd5d3034bd43b9ccf1a74630cd3
ericlau00 <sharkguy418@gmail.com>
2019-08-29 17:06:44 -0400

    update

--- Accumulator.java
+++ Accumulator.java
@@ -17,46 +17,61 @@ public class Accumulator {
         String result = "";
 
         for( int el = 0; el < list.size(); el++)
-            result += // dummy for incremental development
-                      list.get( el) + " ";
+            // result += // dummy for incremental development
+                      // list.get( el) + " ";
 
             /* 3.  Stumbling block 0
                Java protects a programmer against applying a method to
                elements in list when some elements of the list might
                omit support for a particular operation.
 
-               This protection is implemented by the ___________ (compiler? JVM?)
+               This protection is implemented by the compiler (compiler? JVM?)
                The following code violates the restriction:
               */
             // [code that violates the restriction]
 
+            // if (list.get(el).startsWith(prefix)) {
+                // result += list.get(el) + " ";
+            // }
             /*
-             predicted error message:
+             predicted error message: an error message saying that el does not have the particular method. 
 
              actual error message:
+Accumulator.java:33: error: cannot find symbol
+            if (list.get(el).startsWith(prefix)) {
+                            ^
+  symbol:   method startsWith(String)
+  location: class Object
+1 error
              */
 
 
             /* 4.  Workaround 0
                A programmer should expect there to be a way to
                work around the stumbling block, because
-               the ___________ (compiler? JVM?)
+               the JVM (compiler? JVM?)
                knows the type of an element.
 
-               Java's ___________ operator identifies the type
-               of an element to the ___________ (compiler? JVM?).
+               Java's instanceof operator identifies the type
+               of an element to the JVM (compiler? JVM?).
              */
 
 
             /* 5.  Stumbling block 1
                However, use of the operator alone is insufficient,
-               because the ___________ (compiler? JVM?)
+               because the compiler (compiler? JVM?)
                objects to the following code that adds use of
                the operator to the code from Stumbling block 0:
              */
 
             // [code that illustrates the use of the operator]
 
+            if (list.get(el) instanceof String) {
+                String s = list.get(el);
+                if (list.get(el).startsWith(prefix)) {
+                    result += s + " ";
+                }
+            }
             /*
              predicted error message:
 
